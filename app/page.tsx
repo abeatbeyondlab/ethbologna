cb7dcff19fc2b2de40a59cb913e9694d6c0a1f44
abeatbeyondlab <142979443+abeatbeyondlab@users.noreply.github.com>
2023-12-05 20:18:02 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           ETHBologna meetups coming very soon!&nbsp;
-         <code className="text-emerald-50 font-mono font-bold">elegram group here  &nbsp; <a target="_blank" href="https://t.me/ethbologna">@ethbologna</a></code>
+         <code className="text-emerald-50 font-mono font-bold">Telegram group here  &nbsp; <a target="_blank" href="https://t.me/ethbologna">@ethbologna</a></code>
         </p>
       </div>
     </main>
